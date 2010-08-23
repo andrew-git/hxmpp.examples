12d2865c998907337b891af7a4a9f49119ea1d99
tong <sdk@disktree.net>
2010-08-23 15:40:30 +0200

    added non-SASL auth example

---
+++ nonSASL/App.hx
@@ -0,0 +1,27 @@
+
+/**
+	Example usage of non-sasl client authentication with a xmpp server.
+	This should NOT get used since its not secure.
+*/
+class App {
+	
+	static function main() {
+		var cnx = new jabber.SocketConnection( "127.0.0.1", 5222 );
+		var s = new jabber.client.Stream( cnx );
+		s.onOpen = function() {
+			trace("XMPP stream opened");
+			var auth = new jabber.client.NonSASLAuth( s );
+			auth.onSuccess = function() {
+				trace( "Authenticated as "+s.jid.toString() );
+			}
+			auth.onFail = function(?e) {
+				trace( "Failed to authenticate as "+s.jid.toString() );
+			}
+			auth.authenticate( "mypassword", "HXMPP" );
+		}
+		s.onClose = function(?e) {
+			trace("XMPP stream  closed "+e );
+		}
+		s.open( new jabber.JID( "username@disktree" ) );
+	}
+}
