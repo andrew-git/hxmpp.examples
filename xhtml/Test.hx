831748787801a2f94064032a3c9e48f9c4c7bf65
tong <sdk@disktree.net>
2010-08-23 15:55:30 +0200

    added a XHTML example.

---
+++ xhtml/Test.hx
@@ -0,0 +1,19 @@
+
+/**
+	Sends a HTTP link to a buddy.
+*/
+class Test extends ClientBase {
+	
+	override function onLogin() {
+		super.onLogin();
+		stream.sendPresence();
+		var m = new xmpp.Message( "julia@disktree", "LINK" );
+		stream.sendPacket( xmpp.XHTML.attach( m, '<a href="http://disktree.net">DISKTREE.NET</a>' ) );
+	}
+	
+	static function main() {
+		var app = new Test();
+		app.login( "romeo@disktree", "test", "127.0.0.1" );
+	}
+	
+}
