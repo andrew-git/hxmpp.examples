02faa8a9548dbf1c2726bba1ae56ba701dd90d81
tong <sdk@disktree.net>
2010-08-23 16:28:23 +0200

    added a plain javascript example

---
+++ plain_js/Lib.hx
@@ -0,0 +1,11 @@
+
+import jabber.BOSHConnection;
+import jabber.client.Stream;
+import jabber.client.Roster;
+import jabber.client.SASLAuth;
+import jabber.sasl.PlainMechanism;
+
+/**
+	This class imports stuff to build a custom XMPP client library for use from plain javascript.
+*/
+class Lib {}
