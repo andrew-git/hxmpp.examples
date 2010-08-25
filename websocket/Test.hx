ca17acf85a3cc26514e37792677721766060aabd
tong <sdk@disktree.net>
2010-08-25 22:15:25 +0200

    added websocket example

---
+++ websocket/Test.hx
@@ -0,0 +1,16 @@
+
+class Test {
+	
+	static function main() {
+		//var cnx = new jabber.WebSocketConnection( "192.168.0.110", 5666, false );
+		var cnx = new jabber.WebSocketConnection( "192.168.0.110", 5667, true );
+		var stream = new jabber.client.Stream( cnx );
+		stream.onOpen = function(){
+			trace("XMPP stream opened");
+			var auth = new jabber.client.SASLAuth( stream, [cast new jabber.sasl.PlainMechanism()] );
+			auth.authenticate( "test", "HXMPP" );
+		}
+		stream.onClose = function(?e){ trace(e);};
+		stream.open( new jabber.JID( "tong@disktree" ) );
+	}
+}
