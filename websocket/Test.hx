425a81675b3ba142ba0c5b9a32e630ec0670ae73
tong <sdk@disktree.net>
2010-08-26 13:47:34 +0200

    added websocket test for flash

--- websocket/Test.hx
+++ websocket/Test.hx
@@ -2,8 +2,8 @@
 class Test {
 	
 	static function main() {
-		//var cnx = new jabber.WebSocketConnection( "192.168.0.110", 5666, false );
-		var cnx = new jabber.WebSocketConnection( "192.168.0.110", 5667, true );
+		var cnx = new jabber.WebSocketConnection( "192.168.0.110", 5666, false );
+		//var cnx = new jabber.WebSocketConnection( "192.168.0.110", 5667, true );
 		var stream = new jabber.client.Stream( cnx );
 		stream.onOpen = function(){
 			trace("XMPP stream opened");
@@ -13,4 +13,5 @@ class Test {
 		stream.onClose = function(?e){ trace(e);};
 		stream.open( new jabber.JID( "tong@disktree" ) );
 	}
+	
 }
