1fe1e22fad3efaee75d7da745d7cfbeb092fb8a8
tong <sdk@disktree.net>
2010-08-29 22:58:28 +0200

    added last-activity test

---
+++ lastactivity/TestJulia.hx
@@ -0,0 +1,8 @@
+
+class TestJulia extends Test {
+	
+	static function main() {
+		var app = new TestJulia();
+		app.login( "julia@disktree/HXMPP", "test", "127.0.0.1" );
+	}
+}
---
+++ lastactivity/TestRomeo.hx
@@ -0,0 +1,8 @@
+
+class TestRomeo extends Test {
+	
+	static function main() {
+		var app = new TestRomeo();
+		app.login( "romeo@disktree/HXMPP", "test", "127.0.0.1" );
+	}
+}
