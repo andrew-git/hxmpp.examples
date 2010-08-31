0dc4ebf4edfdfeaff69d797e4c20edc360e14e42
tong <sdk@disktree.net>
2010-08-31 20:48:59 +0200

    added plain nodejs example usage

---
+++ plain_nodejs/test.js
@@ -0,0 +1,20 @@
+
+require("./hxmpp");
+
+var cnx = new jabber.SocketConnection( "127.0.0.1" );
+var stream = new jabber.client.Stream( cnx );
+stream.onOpen = function(){
+	console.log("XMPP stream opened");
+	var auth = new jabber.client.SASLAuth( stream, [new jabber.sasl.PlainMechanism()] );
+	auth.onSuccess = function() {
+		haxe.Log.trace("Authenticated as "+stream.jid.toString());
+	}
+	auth.onFail = function(e) {
+		haxe.Log.trace("Login failed");
+	}
+	auth.authenticate("test","HXMPP");
+}
+stream.onClose= function(e){
+	console.log("XMPP stream closed");
+}
+stream.open( new jabber.JID( "romeo@disktree" ) );
