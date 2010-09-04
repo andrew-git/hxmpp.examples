90b0b14d9a5bd48ec1b76a34065bc38412a65770
tong <sdk@disktree.net>
2010-09-04 19:19:23 +0200

    added air test/example

---
+++ air/Test.hx
@@ -0,0 +1,64 @@
+
+import jabber.client.Stream;
+
+class Test {
+	
+	var pass : String;
+	var stream : Stream;
+	
+	function new() {}
+	
+	function login( jid : String, pass : String, host : String, secure : Bool ) {
+		var _jid = new jabber.JID( jid );
+		var _host = ( host == null ) ? _jid.domain : host;
+		this.pass = pass;
+		var cnx : jabber.stream.Connection = if( secure )
+			cast new jabber.SecureSocketConnection( _host );
+		else
+			cast new jabber.SocketConnection( _host );
+		stream = new Stream( cnx );
+		stream.onOpen = onStreamOpen;
+		stream.onClose = onStreamClose;
+		stream.open( _jid );
+	}
+	
+	function onStreamOpen() {
+		var mechs = new Array<jabber.sasl.TMechanism>();
+		mechs.push( new jabber.sasl.PlainMechanism()  );
+		var auth = new jabber.client.SASLAuth( stream, mechs );
+		auth.onSuccess = onLogin;
+		auth.onFail = onLoginFail;
+		auth.authenticate( pass, stream.jid.resource );
+	}
+	
+	function onStreamClose( ?e ) {
+		if( e == null )
+			trace( "XMPP stream closed ["+stream.jid+"]" );
+		else
+			trace( "XMPP stream error ["+e+"]" );
+	}
+	
+	function onLogin() {
+		new jabber.PresenceListener( stream, function(p){} );
+		new jabber.MessageListener( stream, function(m){} );
+		stream.sendPresence();
+	}
+	
+	function onLoginFail( ?e ) {
+		trace( "Authentication failed ["+stream.jid+"]", "warn" );
+	}
+	
+	static function main() {
+		#if flash
+		flash.Lib.current.stage.scaleMode = flash.display.StageScaleMode.NO_SCALE;
+		flash.Lib.current.stage.align = flash.display.StageAlign.TOP_LEFT;
+		#end
+		var app = new Test();
+		// Mind! ...
+		// .. air does not allow to make secure socket connections to servers with self signed certs
+		app.login( "romeo@disktree/HXMPP", "test", "127.0.0.1", false );
+		// .. google allow secure socket connections on port 5223 only
+		//app.login( "username@gmail.com/HXMPP", "test", "talk.google.com", true );
+	}
+	
+}
