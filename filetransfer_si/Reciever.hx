fa4304858932086d3e8a95d0a5fd3da5ec877eaa
tong <sdk@disktree.net>
2010-09-23 00:03:52 +0200

    added filetransfer test

---
+++ filetransfer_si/Reciever.hx
@@ -0,0 +1,137 @@
+
+import haxe.io.Bytes;
+#if neko
+import neko.io.File;
+#elseif cpp
+import cpp.io.File;
+#elseif php
+import php.io.File;
+#elseif nodejs
+import js.io.File;
+#end
+import jabber.data.SIListener;
+import jabber.data.IBReciever;
+import jabber.data.DataReciever;
+#if (neko||cpp||nodejs||flash||js)
+import jabber.data.ByteStreamReciever;
+#end
+
+class Reciever extends ClientBase {
+	
+	var fr : DataReciever;
+	var data : haxe.io.BytesBuffer; // recieved data
+	var bytesRecieved : Int;
+	
+	override function onLogin() {
+		
+		new jabber.ServiceDiscoveryListener( stream );
+		new jabber.MessageListener( stream, function(m:xmpp.Message){} );
+		new jabber.PresenceListener( stream, onPresence );
+		
+		var ft_listener = new SIListener( stream, onDataTransferRequest );
+		ft_listener.onFail = onDataTransferNegotiationFail;
+		
+		#if (neko||cpp||nodejs||flash||js)
+	//	ft_listener.methods.push( new ByteStreamReciever(stream) );
+		#end
+		ft_listener.methods.push( new IBReciever(stream) );
+		
+		stream.sendPresence();
+	}
+	
+	function onPresence( p : xmpp.Presence ) {
+	}
+
+	function onDataTransferNegotiationFail( info : String ) {
+		trace( "Data transfer negotiation failed: "+info );
+	}
+
+	function onDataTransferRequest( fr : DataReciever ) {
+		trace( "Data transfer request: "+fr.initiator+"\n"+
+			   "Filename: "+fr.file.name+"\n"+
+			   "Size: "+fr.file.size+" bytes\n"+
+			   "Hash: "+fr.file.hash+"\n"+
+			   "Description: "+fr.file.desc+"\n"+
+			   "Range: "+(fr.file.range!=null) );
+		data = new haxe.io.BytesBuffer();
+		bytesRecieved = 0;
+		this.fr = fr;
+		fr.onProgress = onDataTransferProgress;
+		fr.onComplete = onDataTransferComplete;
+		fr.onFail = onDataTransferFail;
+		//fr.accept( true, { offset : 4, length : 9 } );
+		//fr.accept( true, { offset : 4, length : null } );
+		fr.accept( true );
+	}
+	
+	function onDataTransferProgress( bytes : Bytes ) {
+		this.data.add( bytes );
+		bytesRecieved += bytes.length;
+		trace( "Filetransfer progress: "+Std.int(bytesRecieved/1024)+"kb "+Std.int(bytesRecieved/fr.file.size*100)+"%" );
+	}
+	
+	function onDataTransferComplete() {
+		
+		var bytes = this.data.getBytes();
+		trace( "File transfer complete ["+bytes.length+"]" );
+		if( fr.file.hash != null ) {
+			if( fr.file.hash != jabber.util.MD5.encode( bytes.toString() ) ) {
+				trace( "Hash does not match", "warn" );
+				return;
+			}
+		}
+		
+		
+		#if (neko||cpp||php)
+		var fo = File.write( "__"+fr.file.name, true );
+		fo.write( bytes );
+		fo.flush();
+		fo.close();
+		trace( "File saved." );
+		
+		#elseif nodejs
+		var d = bytes.getData();
+		js.Node.fs.writeFile( "__"+fr.file.name, d, js.Node.BINARY, function(e){
+			if( e == null ) trace( "File saved." );
+		});
+		
+		#elseif flash
+		trace(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+		if( fr.range != null ) return;
+		var ext = fr.file.name.substr( fr.file.name.length-3 );
+		if( ext == "png" || ext == "jpg" || ext == "gif" ) {
+			trace(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> "+bytes.length );
+			var l = new flash.display.Loader();
+			l.loadBytes( bytes.getData() );
+			flash.Lib.current.addChild( l );
+		}
+		
+		#elseif js
+		if( fr.range != null ) return;
+		var ext = fr.file.name.substr( fr.file.name.length-3 );
+		if( ext == "png" || ext == "jpg" || ext == "gif" ) {
+			var s = "data:image/"+ext+";base64,"+jabber.util.Base64.removeNullbits( jabber.util.Base64.encodeBytes( bytes ) );
+			var img = js.Lib.document.createElement( "img" );
+			img.setAttribute( "src", s );
+			js.Lib.document.getElementById( "output" ).appendChild( img );
+		}
+		
+		#end
+	}
+	
+	function onDataTransferFail( info : String ) {
+		trace( "File transfer failed: "+info );
+	}
+	
+	static function main() {
+		#if (flash||js)
+		haxe.Firebug.redirectTraces();
+		#if flash
+		flash.Lib.current.stage.scaleMode = flash.display.StageScaleMode.NO_SCALE;
+		flash.Lib.current.stage.align = flash.display.StageAlign.TOP_LEFT;
+		#end
+		#end
+		var app = new Reciever();
+		app.login( "julia@disktree" );
+	}
+}
---
+++ filetransfer_si/Sender.hx
@@ -0,0 +1,100 @@
+
+import jabber.data.SITransfer;
+import jabber.data.IBTransfer;
+#if (neko||cpp||nodejs||air)
+import jabber.data.ByteStreamTransfer;
+import xmpp.file.ByteStreamHost;
+#end
+#if neko
+import neko.io.File;
+#elseif cpp
+import cpp.io.File;
+#elseif php
+import php.io.File;
+#elseif nodejs
+import js.io.File;
+#end
+
+class Sender extends ClientBase {
+	
+	static var FILEPATH = "file.png";
+	static var RECIEVER = "julia@disktree";
+	static var initialized = false;
+	
+	var ft : SITransfer;
+	
+	override function onLogin() {
+		new jabber.MessageListener( stream, function(m:xmpp.Message){} );
+		new jabber.PresenceListener( stream, onPresence );
+		stream.sendPresence();
+	}
+	
+	function onPresence( p : xmpp.Presence ) {
+		var from = jabber.JIDUtil.parseBare( p.from );
+		if( !initialized && from == RECIEVER && p.type == null ) {
+			
+			ft = new SITransfer( stream, p.from );
+			
+			#if (neko||cpp||nodejs||air)
+			var bs = new ByteStreamTransfer( stream, p.from );
+			// TODO bs.addHost( stream.jid.toString(), "192.168.0.110", 7654  );
+			bs.hosts.push( new ByteStreamHost( stream.jid.toString(), "192.168.0.110", 7654 ) );
+	//		ft.methods.push( bs );
+			#end
+			
+			ft.methods.push( new IBTransfer( stream, p.from ) );
+			
+			ft.onProgress = onFileTransferProgress;
+			ft.onComplete = onFileTransferComplete;
+			ft.onFail = onFileTransferFail;
+			
+			try {
+				#if (neko||cpp||php||nodejs||air)
+				//var hash = jabber.util.MD5.encode( File.getBytes( FILEPATH ).toString() );
+				var hash = null;
+				ft.sendFile( FILEPATH, "A test filetransfer", hash, true );
+				
+				#elseif (flash||js)
+				ft.sendData( haxe.Resource.getBytes( "file" ), "file.png" );
+				
+				#end
+				
+			} catch( e : Dynamic ) {
+				trace( e );
+				return;
+			}
+			initialized = true;
+		}
+	}
+	
+	/*
+	function onFileTransferStart() {
+		trace( "Filetransfer started" );
+	}
+	*/
+	
+	function onFileTransferFail( error : String, ?info : String ) {
+		var m = error;
+		if( info != null ) m += " ["+info+"]";
+		trace( "Filetransfer failed: "+m );
+	}
+	
+	function onFileTransferProgress( bytes : Int ) {
+		trace( "Filetransfer progress: "+Std.int(bytes/1024)+"kb "+Std.int(bytes/ft.file.size*100)+"%" );
+	}
+	
+	function onFileTransferComplete() {
+		trace( "File transfer complete ["+ft.file.size+"]" );
+	}
+	
+	static function main() {
+		#if (flash||js) #if !air haxe.Firebug.redirectTraces(); #end #end
+		#if flash
+		flash.Lib.current.stage.scaleMode = flash.display.StageScaleMode.NO_SCALE;
+		flash.Lib.current.stage.align = flash.display.StageAlign.TOP_LEFT;
+		#end
+		var app = new Sender();
+		app.login( "romeo@disktree" );
+	}
+	
+}
