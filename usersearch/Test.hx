5526218c3d6639d3dbc479756921339512437310
tong <sdk@disktree.net>
2010-10-23 12:39:22 +0200

    added usersearch test/example

---
+++ usersearch/Test.hx
@@ -0,0 +1,32 @@
+
+class Test extends ClientBase {
+	
+	var usersearch : jabber.UserSearch;
+	
+	override function onLogin() {
+		
+		super.onLogin();
+		stream.sendPresence();
+		
+		usersearch = new jabber.UserSearch( stream );
+		usersearch.onFields = onSearchFields;
+		usersearch.onResult = onSearchResult;
+		usersearch.requestFields( "search.disktree" );
+	}
+	
+	function onSearchFields( jid : String, f : xmpp.UserSearch ) {
+		trace( "Search fields recieved ["+jid+"]:" );
+		//trace( f );
+		usersearch.search( jid, cast { email : "mail@example.com" } );
+	}
+	
+	function onSearchResult( jid : String, f : xmpp.UserSearch ) {
+		trace( "Search result recieved ["+jid+"]" );
+		for( i in f.items ) trace( i );
+	}
+	
+	static function main() {
+		new Test().login( "romeo@disktree" );
+	}
+
+}
