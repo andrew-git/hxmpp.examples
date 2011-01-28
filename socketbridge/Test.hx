f3f1230f6ad8a07af21a64722b110d0e860fa1c4
tong <sdk@disktree.net>
2011-01-28 11:41:15 +0100

    minor

--- socketbridge/Test.hx
+++ socketbridge/Test.hx
@@ -14,7 +14,11 @@ class Test {
 		//var swf = "../../hxmpp/util/socketbridge/socketbridge.swf";
 		untyped swfobject.embedSWF( swf, "socketbridge", "600", "320", "9" );
 		
-		SocketConnection.init( "socketbridge", function() {
+		SocketConnection.init( "socketbridge", function(e:String) {
+			if( e != null ) {
+				trace(e,"error");
+				return;
+			}
 			var cnx = new SocketConnection( "127.0.0.1", 5222, true );
 			var stream = new jabber.client.Stream( cnx );
 			stream.onOpen = function(){
