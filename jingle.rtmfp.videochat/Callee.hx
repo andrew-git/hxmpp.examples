661ee654b36928c1632ca6de200bf38f7f05e942
tong <sdk@disktree.net>
2011-02-01 18:29:50 +0100

    added RTMFP one to one videochat test/example

---
+++ jingle.rtmfp.videochat/Callee.hx
@@ -0,0 +1,115 @@
+
+import flash.events.MouseEvent;
+import flash.events.NetStatusEvent;
+import flash.net.NetStream;
+import jabber.jingle.RTMFPListener;
+import jabber.jingle.RTMFPResponder;
+import Client;
+
+class Callee extends Client {
+	
+	static var CALLER = "romeo@disktree";
+	
+	var responder : RTMFPResponder;
+	
+	var btn_accept : Button;
+	var btn_deny : Button;
+	
+	override function onLogin() {
+		super.onLogin();
+		new jabber.MessageListener( stream, function(m){} );
+		new RTMFPListener( stream, onJingleRequest );
+		stream.sendPresence();
+	}
+
+	override function onPresence( p : xmpp.Presence ) {
+		var from = jabber.JIDUtil.parseBare( p.from );
+		if( from == CALLER ) {
+			if( p.type == null )
+				info.text = "WAITING FOR ROMEO TO CALL";
+			//...
+		}
+	}
+	
+	function onJingleRequest( r : RTMFPResponder ) {
+		
+		trace( "Jingle request: "+r.entity, "info" );
+		
+		responder = r;
+		//responder.cirrus_key = 
+		responder.onInit = onJingleInit;
+		responder.onFail = onJingleFail;
+		responder.onEnd = onJingleEnd;
+		
+		info.text = "INCOMING CALL FROM "+r.entity;
+		
+		btn_accept = new Button( "ACCEPT" );
+		btn_accept.addEventListener( MouseEvent.MOUSE_DOWN, mouseDownAcceptCall );
+		menu.addChild( btn_accept );
+		btn_deny = new Button( "DENY" );
+		btn_deny.x = btn_accept.x + btn_accept.width+1;
+		btn_deny.addEventListener( MouseEvent.MOUSE_DOWN, mouseDownDenyCall );
+		menu.addChild( btn_deny );
+	}
+	
+	function mouseDownAcceptCall( e : MouseEvent ) {
+		btn_accept.visible = btn_deny.visible = false;
+		video.visible = true;
+		responder.accept( true );
+	}
+	
+	function mouseDownDenyCall( e : MouseEvent ) {
+		btn_accept.visible = btn_deny.visible = false;
+		info.text = "YOU DENIED THE CALL";
+		responder.accept( false );
+	}
+	
+	function onJingleInit() {
+		
+		trace("Jingle init","info");
+		
+		var peer : Dynamic = {};
+		peer.onPeerConnect = function( ns : flash.net.NetStream ) : Bool {
+			trace( "Peer connected [farID: "+ns.farID+"]", "info" );
+			return true;
+		}
+		peer.onImageData = function( data : Dynamic ) {
+			trace( "onImageData "+data );
+		}
+		var ns = new NetStream( responder.transport.nc, responder.transport.id );
+		ns.client = peer;
+		ns.addEventListener( NetStatusEvent.NET_STATUS, function(e){trace("########### "+e.info.code);} );
+		video.attachNetStream( ns );
+		ns.play(responder.transport.pubid);
+		
+		btn_hangup.visible = true;
+		info.text = "ROMEO IS TALKING";
+	}
+	
+	function onJingleFail( e : String ) {
+		trace(e,"warn");
+		info.text = "JINGLE FAILED: "+e;
+	}
+	
+	function onJingleEnd( r : xmpp.jingle.Reason ) {
+		trace("Jingle end: "+r );
+		info.text = "CALL ENDED: "+r ;
+		btn_hangup.visible = false;
+	}
+	
+	override function hangup() {
+		responder.terminate();
+		info.text = "YOU ENDED THE SESSION";
+		btn_hangup.visible = false;
+		btn_deny.visible = false;
+		video.visible = false;
+	}
+	
+	static function main() {
+		haxe.Firebug.redirectTraces();
+		flash.Lib.current.stage.scaleMode = flash.display.StageScaleMode.NO_SCALE;
+		flash.Lib.current.stage.align = flash.display.StageAlign.TOP_LEFT;
+		new Callee().login( "julia@disktree/HXMPP", "test", "192.168.0.110" );
+	}
+	
+}
---
+++ jingle.rtmfp.videochat/Caller.hx
@@ -0,0 +1,153 @@
+
+import flash.events.MouseEvent;
+import flash.events.NetStatusEvent;
+import flash.events.StatusEvent;
+import flash.media.Camera;
+import flash.media.Microphone;
+import flash.media.SoundCodec;
+import flash.media.Video;
+import flash.net.NetStream;
+import jabber.jingle.RTMFPCall;
+import jabber.jingle.io.RTMFPOutput;
+import Client;
+
+class Caller extends Client {
+	
+	static var CALLEE = "julia@disktree";
+	
+	var mic : Microphone;
+	var cam : Camera;
+	var jingle : RTMFPCall;
+	var btn_call : Button;
+	var callTimer : haxe.Timer;
+	
+	var ns : flash.net.NetStream;
+	
+	override function onLogin() {
+		
+		super.onLogin();
+		
+		mic = Microphone.getMicrophone();
+		if( mic == null ) {
+			trace("no mic found");
+			return;
+		}
+		mic.setUseEchoSuppression( true );
+		mic.codec = SoundCodec.SPEEX;
+		mic.setSilenceLevel( 0 );
+		mic.framesPerPacket = 1;
+		mic.gain = 70;
+		
+		cam = Camera.getCamera();
+		if( cam == null ) {
+			trace("no cam found");
+			return;
+		}
+		cam.addEventListener( StatusEvent.STATUS, onCamStatus );
+		cam.setMode( 160, 120, 15 );
+		cam.setQuality( 0, 90 );
+		
+		video.attachCamera( cam );
+		
+		btn_call = new Button( "CALL JULIA" );
+		btn_call.addEventListener( MouseEvent.MOUSE_DOWN, mouseDownCall );
+		btn_call.visible = false;
+		menu.addChild( btn_call );
+		
+		stream.sendPresence();
+	}
+	
+	override function onPresence( p : xmpp.Presence ) {
+		if( jingle == null ) {
+			var from = jabber.JIDUtil.parseBare( p.from );
+			if( from == CALLEE ) {
+				if( p.type == null ) {
+					btn_call.visible = true;
+					info.text = "CLICK THE BUTTON TO CALL JULIA";
+				} else {
+					//TODO....
+					info.text = "JULIA IS GONE";
+					btn_call.text = "CALL JULIA";
+					btn_call.visible = false;
+					btn_hangup.visible = false;
+				}
+			}
+		}
+	}
+	
+	function mouseDownCall( e : MouseEvent ) {
+		btn_call.visible = false;
+		info.text = "CALLING JULIA .. STAND BY";
+	//	RTMFPCall.cirrus_key = Client.CIRRUS_KEY;
+		jingle = new RTMFPCall( stream, "julia@disktree/HXMPP" );
+		var transport = new RTMFPOutput( Client.CIRRUS_URL+Client.CIRRUS_KEY );
+		jingle.transports.push( transport );
+		jingle.onInit = onJingleInit;
+		jingle.onEnd = onJingleEnd;
+		jingle.onFail = onJingleFail;
+		try jingle.init() catch( e : Dynamic ) {
+			trace(e);
+			return;
+		}
+		callTimer = new haxe.Timer( 10000 );
+		callTimer.run = onCallTimer;
+	}
+	
+	function onCallTimer() {
+		callTimer.stop();
+		jingle.terminate( xmpp.jingle.Reason.timeout );
+	}
+	
+	function onJingleInit() {
+	
+		callTimer.stop();
+		trace( "Jingle init", "info" );
+		
+		var peer : Dynamic = {};
+		peer.onPeerConnect = function( ns : flash.net.NetStream ) : Bool {
+			trace( "Peer connected [farID: "+ns.farID+"]", "info" );
+			return true;
+		}
+		peer.onImageData = function( data : Dynamic ) {
+			trace( "onImageData" );
+			var l = new flash.display.Loader();
+			l.loadBytes( data.data );
+			flash.Lib.current.addChild( l );
+		}
+		ns = new NetStream( jingle.transport.nc, NetStream.DIRECT_CONNECTIONS );
+		ns.addEventListener( NetStatusEvent.NET_STATUS, function(e){trace("########### "+e.info.code);} );
+		ns.client = peer;
+		ns.attachCamera( cam );
+		ns.attachAudio( mic );
+		ns.publish( jingle.pubid );
+		
+		btn_hangup.visible = true;
+		info.text = "TALKING TO JULIA";
+	}
+	
+	function onJingleEnd( r : xmpp.jingle.Reason ) {
+		trace( "Jingle end "+r, "info" );
+		info.text = "CALL ENDED: "+r ;
+		btn_call.visible = true;
+	}
+	
+	function onJingleFail( info : String ) {
+		trace( "Jingle failed: "+info, "warn" );
+	}
+	
+	override function hangup() {
+		info.text = "ENDING SESSION ..";
+		btn_hangup.visible = false;
+		jingle.terminate();
+	}
+	
+	function onCamStatus( e : StatusEvent ) {
+		//trace(e);
+	}
+	
+	static function main() {
+		haxe.Firebug.redirectTraces();
+		var app = new Caller();
+		app.login( "romeo@disktree/HXMPP", "test", "192.168.0.110" );
+	}
+}
