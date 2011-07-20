73beb08c9aeae23c9eb0b3d52f3cb90fa64fdd4e
tong <sdk@disktree.net>
2011-07-20 12:57:02 +0200

    added client account unregister example

--- accountregister/Test.hx
+++ accountregister/Test.hx
@@ -1,16 +1,18 @@
 
 /**
-	Register an new xmpp client account.
+	Register/Unregister xmpp client account.
 */
 class Test {
-
+	
+	static var node = 'testaccount';
+	static var pass = 'mypassword';
+	
 	static function main() {
-		
-		var node = 'testaccount';
-		var pass = 'mypassword';
-		var email = 'node@example.com';
-		var name = 'Captain Kirk';
-		
+		register();
+		//unregister();
+	}
+	
+	static function register() {
 		var cnx = new jabber.SocketConnection( 'localhost' );
 		var stream = new jabber.client.Stream( cnx );
 		stream.onOpen = function() {
@@ -23,7 +25,7 @@ class Test {
 				trace( e.toString() );
 				stream.close( true );
 			}
-			acc.register( new xmpp.Register( node, pass, email, name ) );
+			acc.register( new xmpp.Register( node, pass, 'node@example.com', 'Captain Kirk' ) );
 		}
 		stream.onClose = function(?e) {
 			if( e != null ) {
@@ -31,6 +33,33 @@ class Test {
 			}
 		}
 		stream.open( null );
+	}
+	
+	static function unregister() {
+		var cnx = new jabber.SocketConnection( 'localhost' );
+		var stream = new jabber.client.Stream( cnx );
+		stream.onOpen = function() {
+			var auth = new jabber.client.Authentication( stream, [new jabber.sasl.PlainMechanism()] );
+			auth.onSuccess = function() {
+				var acc = new jabber.client.Account( stream );
+				acc.onRemove = function() {
+					trace( 'Account successfully removed' );
+					stream.close( true );
+				}
+				acc.onError = function( e ) {
+					trace( e.toString() );
+					stream.close( true );
+				}
+				acc.remove();
+			}
+			auth.authenticate( pass, 'HXMPP' );
+		}
+		stream.onClose = function(?e) {
+			if( e != null ) {
+				trace( "XMPP stream error: "+e );
+			}
+		}
+		stream.open( new jabber.JID( node+'@disktree' ) );
 	}
 	
 }
