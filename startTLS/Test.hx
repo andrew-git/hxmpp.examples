2ab1e2d9a14ec69816ff5ce7f303e0d34143b58c
tong <sdk@disktree.net>
2011-07-20 22:22:37 +0200

    fixed the startTLS example (js/socketbridge_tls)

--- startTLS/Test.hx
+++ startTLS/Test.hx
@@ -1,31 +1,50 @@
 
 class Test {
 	
+	static var ip = "127.0.0.1";
+	static var jid = "romeo@disktree";
+	static var password = "test";
+	
 	static function main() {
 		
 		#if flash
 		flash.Lib.current.stage.scaleMode = flash.display.StageScaleMode.NO_SCALE;
 		flash.Lib.current.stage.align = flash.display.StageAlign.TOP_LEFT;
-		haxe.Firebug.redirectTraces();
 		#end
 		
-		var ip = "127.0.0.1";
-		var jid = "hxmpp@disktree";
+		#if !nodejs
+		#if (flash||js)
+		if( haxe.Firebug.detect() ) haxe.Firebug.redirectTraces();
+		#end
+		#end
 		
+		#if (js&&!nodejs)
+		untyped swfobject.embedSWF('../../hxmpp/util/socketbridge/socketbridge_tls.swf','socketbridge','0','0','10');
+		jabber.SocketConnection.init( 'socketbridge', function(error:String) {
+			connect();
+		}, 500 );
+		
+		#else
+		connect();
+		
+		#end
+	}
+	
+	static function connect() {
 		var cnx = new jabber.SocketConnection( ip );
 		var stream = new jabber.client.Stream( cnx );
 		stream.onOpen = function() {
-			trace("XMPP stream opened");
+			trace( "XMPP stream opened" );
 			var auth = new jabber.client.Authentication( stream, [cast new jabber.sasl.PlainMechanism()] );
 			auth.onSuccess = function() {
 				stream.sendPresence();
 				new jabber.client.VCard( stream ).load();
 			}
-			auth.authenticate( "test", "HXMPP" );
+			auth.authenticate( password, "HXMPP" );
 		}
 		stream.onClose = function(?e) {
-			trace("XMPP stream closed");
-			trace(e);
+			trace( "XMPP stream closed", ( e != null ) ? "error" : "info" );
+			if( e != null ) trace( e, "error" );
 		}
 		stream.open( new jabber.JID( jid ) );
 	}
